7444d50c651adceb853d52b631ff73be27af0e12
AnhPT <tuananh200898@gmail.com>
2019-03-09 14:23:57 +0700

    commit for demo

---
+++ precache-manifest.b9c8d6194d0af54ca72a781020f7f03e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0ff36048f3304a60d53c",
+    "url": "/static/js/main.4b8ad30a.chunk.js"
+  },
+  {
+    "revision": "424faad7c0987d798730",
+    "url": "/static/js/2.4e426701.chunk.js"
+  },
+  {
+    "revision": "0ff36048f3304a60d53c",
+    "url": "/static/css/main.12daf7ff.chunk.css"
+  },
+  {
+    "revision": "9e6d28525ea4ee0e4b550b591d8903b2",
+    "url": "/index.html"
+  }
+];
